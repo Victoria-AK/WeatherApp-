63fe7452cb10d5afa65a7d22f4d3bc4493b674f4
Victoria-AK <toriknights07@gmail.com>
2021-05-28 15:22:22 +0100

    Structure change to col

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ days.forEach(function(day){
 })
 forecastHTML= forecastHTML + `
 
-    <div class="col">
+    <div class="col-2">
         <div class="weather-forecast-date">Tue</div>
         
         <img src="http://openweathermap.org/img/wn/10d@2x.png"
