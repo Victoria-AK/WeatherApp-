878ccbc9fdca758c24e2f9b2dd4682e104a65cf6
Victoria-AK <toriknights07@gmail.com>
2021-05-28 16:36:36 +0100

    Updated col

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ let forecastHTML=`<div class="row">`;
 
 days.forEach(function(day){
   forecastHTML= forecastHTML + 
-  `<div class="col">
+  `<div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         
         <img src="http://openweathermap.org/img/wn/10d@2x.png"
