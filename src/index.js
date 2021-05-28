4e4a1fada8a47cc08997fa7ff1f12a43123227e4
Victoria-AK <toriknights07@gmail.com>
2021-05-28 15:49:42 +0100

    Syntax addition

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ days.forEach(function(day){
         <img src="http://openweathermap.org/img/wn/10d@2x.png"
         alt =""
         width="42">
+        />
         
         <div class="weather-forecsr-temperature">
         <span class="weather-forecast-temperature-max">
